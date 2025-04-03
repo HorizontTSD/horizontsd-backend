8b95bfa8b7c79fe79b345bf30033af15856b4d0d
Oleg_Borr <ser.garmadon@gmail.com>
2025-04-03 22:42:16 +0300

    Ввод в приложение глобальных переменных и модели данных от клиентаю

--- src/server.py
+++ src/server.py
@@ -1,41 +1,21 @@
-from typing import Annotated, List
-
-# import pandas as pd
-import uvicorn
-from fastapi import FastAPI, HTTPException, Body
-from fastapi.middleware.cors import CORSMiddleware
-
-from src.config import logger, public_or_local
-from src.models.schemes import HellowRequest
-from src.utils.greeting import hellow_names
-
-if public_or_local == 'LOCAL':
-    url = 'http://localhost'
-else:
-    url = 'http://11.11.11.11'
-
-origins = [
-    url
-]
-
-app = FastAPI(docs_url="/template_fast_api/v1/", openapi_url='/template_fast_api/v1/openapi.json')
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-"""Функциональная часть отвечающая за отправку запроса на сайт и получение ответа в формате JSON"""
-from fastapi import FastApi
+# Импортируем необходимые библиотеки.
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import httpx
 
-app = FastAPI()
+# глобальные переменные приложения
+app = FastAPI
+EXT_URL  = "http://77.37.136.11:8501"
+
+# Форма для заполнения клиентом(Model)
+class UserData(BaseModel):
+    """Ссюда необходимо занести данные которые будут подаваться со стороны клинта
+    имена, числа, какие то другие данне необходимые для расчетов"""
+    data: dict
+    pass
 
 @app.post("/")
-async def graph_forecast (data: dict):
+async def my_request_rasponse(data: dict):
     async with httpx.AsyncClient() as client:
-        response = await client.post("http://77.37.136.11:8501/", params=data)
+        response = await client.get("http://77.37.136.11:8501/", params=data)
         return response.json()
