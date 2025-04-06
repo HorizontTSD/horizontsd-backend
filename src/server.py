6e5bb913b5de32edaa448c9bccb21e68066fce2b
Oleg_Borr <ser.garmadon@gmail.com>
2025-04-06 22:06:54 +0300

    Обработчик маршрута

--- src/server.py
+++ src/server.py
@@ -5,7 +5,7 @@ import httpx
 
 # глобальные переменные приложения
 app = FastAPI
-EXT_URL  = "http://77.37.136.11:8501"
+GORIZONT_URL  = "http://77.37.136.11:8501"
 
 # Форма для заполнения клиентом(Model)
 class UserData(BaseModel):
@@ -14,8 +14,19 @@ class UserData(BaseModel):
     data: dict
     pass
 
-@app.post("/")
-async def my_request_rasponse(data: dict):
-    async with httpx.AsyncClient() as client:
-        response = await client.get("http://77.37.136.11:8501/", params=data)
-        return response.json()
+@app.post("/process")
+async def process_data(request: ClientRequest):
+    """В данной функции(обработчик маршрута) принимаю запрос данными,
+        так же нужно отправить на внешний API, делает запрос и 
+        возращаем ответ клиенту"""
+   async with httpx.AsyncClient() as client:
+        try:
+            post_response = await client.post(
+                GORIZONT_URL,
+                json={"data": request.data}
+            )
+            post_response.raise_for_status()
+            get_response = await client.get(GORIZONT_URL)
+            get_response.raise_for_status()
+
+            return get_response.json()
